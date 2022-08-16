e871d7053e6d289e81836f76ac9f82381d883c3d
Albert Tang <tangalbert919@yahoo.com>
2022-08-16 15:40:18 -0500

    techpack: camera: Import changes from DE2117_11_C.15
    Change-Id: Ia23473f845140a63d1b3199bb8efa4b1fd884c6b

--- techpack/camera/include/uapi/camera/media/cam_defs.h
+++ techpack/camera/include/uapi/camera/media/cam_defs.h
@@ -10,6 +10,9 @@
 #include <linux/types.h>
 #include <linux/ioctl.h>
 
+#ifndef OPLUS_FEATURE_CAMERA_COMMON
+#define OPLUS_FEATURE_CAMERA_COMMON
+#endif
 
 /* camera op codes */
 #define CAM_COMMON_OPCODE_BASE                  0x100
@@ -893,5 +896,9 @@ struct cam_dump_req_cmd {
 	__s32           link_hdl;
 	__s32           dev_handle;
 };
+
+#ifdef OPLUS_FEATURE_CAMERA_COMMON
+#include "oplus/media/oplus_cam_defs.h"
+#endif
 
 #endif /* __UAPI_CAM_DEFS_H__ */
